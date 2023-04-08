baad7ec767d1123631ff73571ee4b5d73498ddf8
Lee <elcheveremochilero@gmail.com>
2023-04-07 22:15:00 -0500

    Add frame motion for animation

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { AnimationProps, motion } from "framer-motion";
 
 type Props = {
     timeLeft: number;
@@ -6,38 +7,65 @@ type Props = {
     isRunning:boolean;
 }
 
+type CircleStatus = "show" | "hide"
+
 const Circle = ({timeLeft, minutes, isRunning}: Props) => {
-    
-    // 
+
+    const [circleStatus, setCircleStatus] = useState<CircleStatus>("hide")
+
+    // Span
     const maxSpan = 1005;
     const totalTime = minutes * 60;
     const timeLeftPercentage = timeLeft / totalTime;
     const span = maxSpan * timeLeftPercentage
+    const strokeDasharray = `${Math.floor(span)}, 20000`
 
     // Circle color changes from green to red as R increases and G decreases
-    const R = Math.floor(255 - (255 * timeLeftPercentage))
-    const G = Math.floor(255 * timeLeftPercentage + totalTime * 1.5)
+    const R = Math.floor(255 - (255 * timeLeftPercentage ) + 50)
+    const G = Math.floor(255 * timeLeftPercentage + 100)
 
+    // Animation
+    const initial: AnimationProps["initial"] = {
+        opacity: 0,
+        strokeWidth:20,  
+    }
+    
+    const showCircle : AnimationProps["animate"] = {        
+        opacity: 1,
+        strokeDasharray: strokeDasharray,
+        stroke:`rgb(${R},${G},0)`,
+    }
+
+    const hideCircle : AnimationProps["animate"] = {
+        opacity: 0,
+        strokeDasharray: "0, 20000",   
+    }
+    
+    const transition : AnimationProps["transition"]= {
+        duration: 1,  
+        ease: "circOut",
+    }
+    
     useEffect(() => {
-      console.log(timeLeftPercentage);
-      console.log(isRunning)
-    }, [timeLeft]);
-    
-    
+        isRunning
+        ? setCircleStatus("show")
+        : setCircleStatus("hide")
+    },[isRunning])
+
     return (
-        <svg 
+        <motion.svg 
         width={400}
         height={400} 
-        className="absolute" 
-        style={{
-            stroke:`rgb(${R},${G},0)`,
-            strokeWidth:20,
-            fill:"none",
-            strokeDasharray:`${span}, 20000`, // max 1005
-            zIndex:-1,
-        }}>
-            {isRunning && <circle cx={200} cy={200} r={160} transform="rotate(-90,200,200)"/>}
-        </svg> 
+        className="absolute -z-10"
+        >
+           <motion.circle cx={200} cy={200} r={160}     
+            transform="rotate(-90,200,200)"
+            initial={initial}
+            animate={circleStatus === "show" ? showCircle : hideCircle}
+            transition={transition}
+            className="fill-none"
+            />
+        </motion.svg> 
     );
 }
  
